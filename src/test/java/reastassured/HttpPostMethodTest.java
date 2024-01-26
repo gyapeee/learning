fe3cc04e392551cba3dfaea0f41ef054554d8770
David Kormos <david.kormos@lastpass.com>
2024-01-26 12:28:14 +0100

    Added RESTAssured PUT HTTP method test case

--- src/test/java/reastassured/HttpPostMethodTest.java
+++ src/test/java/reastassured/HttpPostMethodTest.java
@@ -14,22 +14,56 @@ public class HttpPostMethodTest {
         .put("username", "admin")
         .put("password", "password123")
         .toString();
+    public static final String POST_PAYLOAD = new JSONObject()
+        .put("firstname", "David")
+        .put("lastname", "Kormos")
+        .put("totalprice", 69)
+        .put("depositpaid", true)
+        .put("bookingdates", new JSONObject()
+            .put("checkin", "2002-02-20")
+            .put("checkout", "2022-02-22")
+        )
+        .put("additionalneeds", "Lunch").toString();
 
-    @Test
-    public void authToken() {
+    public static String getAuthToken() {
+        //@formatter:off
+    Response response = given()
+        .baseUri(Globals.RESTFUL_BROKER_BASE_URL)
+        .contentType(ContentType.JSON)
+        .body(AUTH_PAYLOAD)
+    .when()
+        .post("/auth")
+    .then()
+        .log().all()
+        .extract().response();
+        //@formatter:on
+
+        return response.jsonPath().getString("token");
+    }
+
+    static int getBookingId() {
         //@formatter:off
         Response response = given()
             .baseUri(Globals.RESTFUL_BROKER_BASE_URL)
             .contentType(ContentType.JSON)
-            .body(AUTH_PAYLOAD)
+            .body(POST_PAYLOAD)
+            //.header("Cookie", "token=" + authToken)
+            .log().all()
         .when()
-            .post("/auth")
+            .post("/booking")
         .then()
             .log().all()
+            .assertThat()
+            .statusCode(200)
             .extract().response();
+        //@formatter:on
 
-        //@formatter:off
-        String token = response.jsonPath().getString("token");
-        Assertions.assertNotEquals("",token);
+        return response.jsonPath().getInt("bookingid");
+    }
+
+    @Test
+    public void authToken() {
+        String token = getAuthToken();
+        Assertions.assertNotEquals("", token);
     }
 }
---
+++ src/test/java/reastassured/HttpPutMethodTest.java
@@ -0,0 +1,49 @@
+package reastassured;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class HttpPutMethodTest {
+
+    public static final String PUT_PAYLOAD = new JSONObject()
+        .put("firstname", "David")
+        .put("lastname", "Kormos")
+        .put("totalprice", 6969)
+        .put("depositpaid", true)
+        .put("bookingdates", new JSONObject()
+            .put("checkin", "2002-02-20")
+            .put("checkout", "2022-02-22")
+        )
+        .put("additionalneeds", "Lunch").toString();
+
+
+    @Test
+    public void updateBooking() {
+        int bookingId = HttpPostMethodTest.getBookingId();
+        String accessToken = HttpPostMethodTest.getAuthToken();
+
+        //@formatter:off
+        Response response = given()
+            .baseUri(Globals.RESTFUL_BROKER_BASE_URL)
+            .contentType(ContentType.JSON)
+            .cookie("token", accessToken)
+            .body(PUT_PAYLOAD)
+            .log().all()
+        .when()
+            .put("/booking/"+bookingId)
+        .then()
+            .log().all()
+            .assertThat()
+            .statusCode(200)
+            .extract().response();
+        //@formatter:on
+
+        Assertions.assertEquals(6969, response.jsonPath().getInt("totalprice"));
+    }
+
+}
