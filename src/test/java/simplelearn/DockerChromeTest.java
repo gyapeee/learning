d64c8ee1d3df149a8a785382bd82e8459c281d12
grosh <kormosabel2001@gmail.com>
2024-02-07 14:19:21 +0100

    example test code for WDM running in docker with vnc enabled and forced browser version to 119

---
+++ src/test/java/simplelearn/DockerChromeTest.java
@@ -0,0 +1,47 @@
+package simplelearn;
+
+
+import java.net.URL;
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class DockerChromeVncTest {
+
+    WebDriver driver;
+
+    WebDriverManager wdm = WebDriverManager.chromedriver().browserInDocker()
+            .enableVnc().browserVersion("119");
+
+    @BeforeEach
+    void setupTest() {
+        driver = wdm.create();
+    }
+
+    @AfterEach
+    void teardown() {
+        wdm.quit();
+    }
+
+    @Test
+    void test() throws Exception {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        Assertions.assertTrue(driver.getTitle().contains("Selenium WebDriver"));
+
+        // Verify URL for remote session
+        URL noVncUrl = wdm.getDockerNoVncUrl();
+        Assertions.assertNotNull(noVncUrl);
+
+        System.out.println(noVncUrl);
+
+        // Pause for manual inspection
+        Thread.sleep(Duration.ofSeconds(60).toMillis());
+    }
+
+}
